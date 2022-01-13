77785cc0cff372d48757d1c59c5e1ea9137aa5c9
Sultan Achmad <sultangendut@gmail.com>
2022-01-13 16:35:04 +0700

    feat: done parser feature select

--- backend-assessment/lib/parseFeatureNode.js
+++ backend-assessment/lib/parseFeatureNode.js
@@ -1,90 +1,109 @@
-function binaryExpr(obj, arr) {
-    const pattern = /[a-zA-Z]/g;
-    const operator = obj.operator;
-    const left = typeCheck(obj.left, arr);
-    const right = typeCheck(obj.right, arr);
-    if (pattern.test(operator)) {
-        arr.unshift(operator);
-    } else {
-        arr.push(operator + "_" + left + "_" + right);
-    }
-}
-
 function aliasString(prefix, aliases) {
     return `${prefix}_as_${aliases}`;
 }
 
 function typeCheck(obj, arr = []) {
+    if (obj == null) return null;
     const type = obj.type ? obj.type : null;
+    const pattern = /[a-zA-Z]/g;
     if (type == "column_ref") {
+        /* {
+            "type": "column_ref",
+            "table": null,
+            "column": "nama"
+        } */
         return obj.column;
     } else if (type == "select") {
         return "SubQuery";
-    } else if (type == "binary_expr") {
-        /* "type": "binary_expr",
-            "operator": "=",
-            "left": {
-                "type": "column_ref",
-                "table": null,
-                "column": "kelas"
-            },
-            "right": {
-                "type": "single_quote_string",
-                "value": "ti-4c"
-            } */
-        binaryExpr(obj, arr);
-        return arr;
-    } else if (type == "number") {
+    } else if (type == "param" || type == "null" || type == "bool" || type == "star") {
+        // { type: 'param', value: 'my_param' }
+        return obj.value;
+    } else if (type.includes("string") || type == "number") {
         /* {
             "type": "number",
             "value": 1123144
-        }, */
-        // do nothing
-    } else if (type == "function") {
-        /*         {
-            type: 'function',
-            name: func,
-            over: null,
-            args: {
-              type: 'expr_list',
-              value: []
-            } */
-    } else if (type == "aggr_func") {
-        /* {
-            type: 'aggr_func',
-            name: 'SUM',
-            over: null,
-            args: { expr: { type: 'column_ref', table: null, column: 'col2' } }
-          } */
-    } else if (type == "expr_list") {
-        /* type : 'expr_list',
-            value : [
-              { type: 'number', value: 1 },
-              { type: 'single_quote_string', value: 't' }
-            ] */
-    } else if (type == "param" || type == "null" || type == "bool" || type == "origin") {
-        // { type: 'param', value: 'my_param' }
-        return obj.value;
+        }, OR 
+        {
+            "type": "single_quote_string",
+            "value": "tomi"
+        }*/
+        return "Constant";
+    } else if (type == "binary_expr") {
+        /* { 
+            "type": "binary_expr",
+            "operator": "AND",
+            "left": {
+                "type": "binary_expr",
+                "operator": "=",
+                "left": {},
+                "right": {}
+            },
+            "right": {}
+        } */
+        const operator = obj.operator;
+        const left = typeCheck(obj.left, arr);
+        const right = typeCheck(obj.right, arr);
+        if (pattern.test(operator) && (operator == "AND" || operator == "OR")) {
+            arr.unshift(operator);
+        } else {
+            arr.push(operator + "_" + left + "_" + right);
+        }
+        return arr;
     } else if (type == "unary_expr") {
         /* {
             type: 'unary_expr',
             operator: 'NOT',
             expr: { type: 'bool', value: true }
           } */
-    } else if (type.includes("string")) {
-        return "constant";
+        const operator = obj.operator;
+        if (!pattern.test(operator)) operator = "Equation";
+        return operator + "_ " + exprCheck(obj.expr);
+    } else if (type == "expr_list") {
+        /* type : 'expr_list',
+        value : [
+          { type: 'number', value: 1 },
+          { type: 'single_quote_string', value: 't' }
+        ] */
+        if (Array.isArray(obj.value)) {
+            arr = obj.value.map((e) => {
+                exprCheck(e);
+            });
+        }
+        return arr;
+    } else if (type == "function" || type == "aggr_func") {
+        /*         {
+            type: 'function',
+            name: func,
+            over: null,
+            args: {
+                type: 'expr_list',
+                value: []
+            } OR
+            {
+            type: 'aggr_func',
+            name: 'SUM',
+            over: null,
+            args: { expr: { type: 'column_ref', table: null, column: 'col2' } }
+          }
+            */
+        let result = exprCheck(obj.args);
+        if (Array.isArray(result)) result.unshift(obj.name);
+        else result = obj.name + "_" + result;
+        return result;
     } else {
         return type;
     }
 }
 
 function exprCheck(exprObj) {
-    if (typeof exprObj == "object") {
+    if (typeof exprObj == "object" && exprObj != null) {
         let objPass = {};
         if ("type" in exprObj) {
             objPass = exprObj;
         } else if ("ast" in exprObj) {
             objPass = exprObj.ast;
+        } else if ("expr" in exprObj) {
+            objPass = exprObj.expr;
         }
         return typeCheck(objPass);
     }
@@ -96,19 +115,21 @@ function getSelectColumns(distinct, columns) {
         return columns;
     } else if (Array.isArray(columns)) {
         columns = columns.map((e) => {
-            return exprCheck(e.expr);
+            return exprCheck(e);
         });
 
         if (distinct) {
             columns = [distinct, ...columns];
         }
+        return columns.flat(Infinity);
+    } else {
+        return columns;
     }
-    return columns;
 }
 
 function getTable(tableArr) {
     if (Array.isArray(tableArr)) {
-        return tableArr.flatMap((e) => {
+        return tableArr.map((e) => {
             let arr = [];
             if (e.join) {
                 arr.push(e.join + "_" + e.table);
@@ -117,7 +138,7 @@ function getTable(tableArr) {
             return arr;
         });
     }
-    return tableArr;
+    return tableArr.flat(Infinity);
 }
 
 function getByStatement(arr) {
@@ -126,16 +147,16 @@ function getByStatement(arr) {
             exprCheck(e);
         });
     }
-    return arr;
+    return arr.flat(Infinity);
 }
 
 function getLimit(obj) {
-    if (typeof obj != "object") return obj;
+    if (obj == null) return obj;
     const separator = obj.separator;
     let value = obj.value.map((e) => {
         return exprCheck(e);
     });
-    return value.unshift(separator);
+    return value.unshift(separator).flat(Infinity);
 }
 
 function parseFeatureNode(ast) {
@@ -146,8 +167,8 @@ function parseFeatureNode(ast) {
         case "select":
             column = getSelectColumns(ast.distinct, ast.columns);
             let from = getTable(ast.from);
-            let where = exprCheck(ast.where);
-            let having = exprCheck(ast.having);
+            let where = ast.where ? [exprCheck(ast.where)].flat(Infinity) : null;
+            let having = ast.having ? [exprCheck(ast.having)].flat(Infinity) : null;
             let groupBy = getByStatement(ast.groupby);
             let orderBy = getByStatement(ast.orderby);
             let limit = getLimit(ast.limit);
