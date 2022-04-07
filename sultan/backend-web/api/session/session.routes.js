78c3bb8bbae936df8529a5fa0518033895f75b37
Sultan Achmad <sultangendut@gmail.com>
2022-04-08 02:33:27 +0700

    fix: order route session prevent wrong route exec

--- sultan/backend-web/api/session/session.routes.js
+++ sultan/backend-web/api/session/session.routes.js
@@ -7,9 +7,9 @@ const sessionSanitizer = require("./session.sanitizer");
 
 router.get("/", sessionController.index);
 router.get("/:id", sessionSanitizer.checkIdOnly, sessionController.show);
-router.post("/:scheduleid", permission("mahasiswa"), sessionSanitizer.checkStore, sessionController.store);
 router.post("/answer", permission("mahasiswa"), sessionSanitizer.checkAnswer, sessionController.answer);
-router.delete("/reset/:id", sessionSanitizer.checkIdOnly, sessionController.reset);
+router.post("/:scheduleid", permission("mahasiswa"), sessionSanitizer.checkStore, sessionController.store);
 router.post("/grade/:id", permission("mahasiswa"), sessionSanitizer.checkIdOnly, sessionController.grade);
+router.delete("/reset/:id", sessionSanitizer.checkIdOnly, sessionController.reset);
 
 module.exports = router;
