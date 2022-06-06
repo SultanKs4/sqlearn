1148479e81c779168165b6812729b3695d461631
Sultan Achmad <sultangendut@gmail.com>
2022-06-06 17:54:51 +0700

    change pm2 to cluster mode

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,17 +4,23 @@ module.exports = {
       name: "sqlearn-be-web",
       cwd: "sultan/backend-web/",
       script: "./server.js",
+      instances: "max",
+      exec_mode: "cluster",
     },
     {
       name: "sqlearn-be-assessment",
       cwd: "backend-assessment/",
       script: "./server.js",
+      instances: "max",
+      exec_mode: "cluster",
     },
     {
       name: "sqlearn-fe",
       cwd: "ilham/frontend-web/",
       script: "node_modules/next/dist/bin/next",
       args: "start -p 5000",
+      instances: "-1",
+      exec_mode: "cluster",
     },
   ],
 };
