d4c4a9292d314d3f29c5dd485569779689284647
ilhamadhim <ilhamm179@gmail.com>
2022-07-22 10:30:51 +0700

    fix : breaking changes middleware next auth solved

--- frontend-web/components/Sider.js
+++ frontend-web/components/Sider.js
@@ -18,89 +18,89 @@ import styles from "../styles/components/Sider.module.css";
 
 const { Sider } = Layout;
 
-const navItemListAdmin = [
-  {
-    key: "daftar-dosen",
-    icon: <TeamOutlined />,
-    onClick: () => router.push("/admin"),
-    label: "Daftar Dosen",
-  },
-  {
-    key: "konfigurasi-penilaian",
-    icon: <ContainerOutlined />,
-    onClick: () => router.push("/admin/konfigurasi-penilaian"),
-    label: "Konfigurasi Penilaian",
-  },
-  {
-    key: "edit-profile",
-    icon: <FormOutlined />,
-    onClick: () => router.push("/admin/edit-profile"),
-    label: "Edit Profile",
-  },
-];
-
-const navItemListDosen = [
-  {
-    key: "jadwal",
-    icon: <CalendarOutlined />,
-    onClick: () => router.push("/dosen"),
-    label: "Jadwal",
-  },
-  {
-    key: "kelas",
-    icon: <LaptopOutlined />,
-    onClick: () => router.push("/dosen/kelas"),
-    label: "Kelas",
-  },
-  {
-    key: "studi-kasus",
-    icon: <DatabaseOutlined />,
-    onClick: () => router.push("/dosen/studi-kasus"),
-    label: "Studi Kasus",
-  },
-  {
-    key: "paket-soal",
-    icon: <CodeSandboxOutlined />,
-    onClick: () => router.push("/dosen/paket-soal"),
-    label: "Paket Soal",
-  },
-  {
-    key: "soal",
-    icon: <FileTextOutlined />,
-    onClick: () => router.push("/dosen/soal"),
-    label: "Soal",
-  },
-  {
-    key: "nilai-mahasiswa",
-    icon: <TeamOutlined />,
-    onClick: () => router.push("/dosen/nilai-mahasiswa"),
-    label: "Nilai Mahasiswa",
-  },
-  {
-    key: "edit-profile",
-    icon: <FormOutlined />,
-    onClick: () => router.push("/dosen/edit-profile"),
-    label: "Ubah Profile",
-  },
-];
-
-const navItemListMhs = [
-  {
-    key: "beranda",
-    icon: <HomeOutlined />,
-    onClick: () => router.push("/mahasiswa"),
-    label: "Beranda",
-  },
-  {
-    key: "edit-profile",
-    icon: <FormOutlined />,
-    onClick: () => router.push("/mahasiswa/edit-profile"),
-    label: "Edit Profile",
-  },
-];
-
 function SiderComponent({ role, collapsed, setCollapsed }) {
   const router = useRouter();
+
+  const navItemListAdmin = [
+    {
+      key: "daftar-dosen",
+      icon: <TeamOutlined />,
+      onClick: () => router.push("/admin"),
+      label: "Daftar Dosen",
+    },
+    {
+      key: "konfigurasi-penilaian",
+      icon: <ContainerOutlined />,
+      onClick: () => router.push("/admin/konfigurasi-penilaian"),
+      label: "Konfigurasi Penilaian",
+    },
+    {
+      key: "edit-profile",
+      icon: <FormOutlined />,
+      onClick: () => router.push("/admin/edit-profile"),
+      label: "Edit Profile",
+    },
+  ];
+
+  const navItemListDosen = [
+    {
+      key: "jadwal",
+      icon: <CalendarOutlined />,
+      onClick: () => router.push("/dosen"),
+      label: "Jadwal",
+    },
+    {
+      key: "kelas",
+      icon: <LaptopOutlined />,
+      onClick: () => router.push("/dosen/kelas"),
+      label: "Kelas",
+    },
+    {
+      key: "studi-kasus",
+      icon: <DatabaseOutlined />,
+      onClick: () => router.push("/dosen/studi-kasus"),
+      label: "Studi Kasus",
+    },
+    {
+      key: "paket-soal",
+      icon: <CodeSandboxOutlined />,
+      onClick: () => router.push("/dosen/paket-soal"),
+      label: "Paket Soal",
+    },
+    {
+      key: "soal",
+      icon: <FileTextOutlined />,
+      onClick: () => router.push("/dosen/soal"),
+      label: "Soal",
+    },
+    {
+      key: "nilai-mahasiswa",
+      icon: <TeamOutlined />,
+      onClick: () => router.push("/dosen/nilai-mahasiswa"),
+      label: "Nilai Mahasiswa",
+    },
+    {
+      key: "edit-profile",
+      icon: <FormOutlined />,
+      onClick: () => router.push("/dosen/edit-profile"),
+      label: "Ubah Profile",
+    },
+  ];
+
+  const navItemListMhs = [
+    {
+      key: "beranda",
+      icon: <HomeOutlined />,
+      onClick: () => router.push("/mahasiswa"),
+      label: "Beranda",
+    },
+    {
+      key: "edit-profile",
+      icon: <FormOutlined />,
+      onClick: () => router.push("/mahasiswa/edit-profile"),
+      label: "Edit Profile",
+    },
+  ];
 
   const onCollapse = (statusCollapse) => setCollapsed(statusCollapse);
   let siderElement;
--- frontend-web/middleware.js
+++ frontend-web/middleware.js
@@ -1,18 +1,43 @@
 import { withAuth } from "next-auth/middleware";
+import { NextRequest, NextResponse } from "next/server";
 
 export default withAuth(
   // `withAuth` augments your `Request` with the user's token.
   function middleware(req) {
-    console.log(req.nextauth.token);
+    if (
+      !req.nextUrl.pathname.includes(req.nextauth.token.userRole) ||
+      req.nextauth.token === null
+    ) {
+      const url = req.nextUrl.clone();
+      url.pathname = "/login";
+      return NextResponse.redirect(url);
+    }
   },
   {
     callbacks: {
-      authorized: ({ token }) =>
-        token?.role === "admin" ||
-        token?.role === "mahasiswa" ||
-        token?.role === "dosen",
+      authorized: ({ token }) => !!token,
     },
   }
 );
 
-export const config = { matcher: ["/admin", "/mahasiswa", "/dosen"] };
+export const config = {
+  matcher: [
+    // Page Admin
+    "/admin",
+    "/admin/edit-profile",
+    "/admin/konfigurasi-penilaian",
+
+    // Page Mahasiswa
+    "/mahasiswa",
+    "/mahasiswa/soal",
+    "/mahasiswa/edit-profile",
+    // Page Dosen
+    "/dosen",
+    "/dosen/kelas",
+    "/dosen/soal",
+    "/dosen/paket-soal",
+    "/dosen/nilai-mahasiswa",
+    "/dosen/studi-kasus",
+    "/dosen/edit-profile",
+  ],
+};
